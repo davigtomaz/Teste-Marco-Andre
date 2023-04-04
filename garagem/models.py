d8404f3d345a71aaaae87bc9af22d55cdd36c56e
Davi <davigabtomaz@gmail.com>
2023-04-04 15:09:51 -0300

    Feito Rest_Framework e adicionado mais models

--- garagem/models.py
+++ garagem/models.py
@@ -12,3 +12,36 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name = "acessório"
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name_plural = "cores"
+
+class Veiculo(models.Model):
+    modelo = models.CharField(max_length=255, default='')
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculo")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculo")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo")
+    ano = models.IntegerField(default=0)
+
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.modelo} (Marca: {self.marca} - Ano: {self.ano} - Cor: {self.cor})"
+    
+    class Meta:
+        verbose_name = "veículo"
